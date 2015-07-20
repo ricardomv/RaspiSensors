1504c431a96e56b9ca2adf8cad2899c0f01a1384
Ricardo Vieira <ricardo.vieira@tecnico.ulisboa.pt>
2015-07-20 17:05:29 +0100

    Detect if FindBCM2835 found the library and display a message

--- cmake/modules/FindBCM2835.cmake
+++ cmake/modules/FindBCM2835.cmake
@@ -11,4 +11,10 @@ find_library (BCM2835_LIBRARY NAMES bcm2835)
 set (BCM2835_LIBRARIES ${BCM2835_LIBRARY} )
 set (BCM2835_INCLUDE_DIRS ${BCM2835_INCLUDE_DIR} )
 
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set BCM2835_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(BCM2835 DEFAULT_MSG
+                                  BCM2835_LIBRARY BCM2835_INCLUDE_DIR)
+
 mark_as_advanced (BCM2835_INCLUDE_DIR BCM2835_LIBRARY )
