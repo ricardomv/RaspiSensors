bc490e85a96429151e11e76ebc22b91970d08885
Ricardo Vieira <ricardo.vieira@tecnico.ulisboa.pt>
2015-07-28 16:45:52 +0100

    Add initial accel and gyro test code
    *Tested: Linux i2c interface working

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "BMP085.h"
 #include "HMC5883L.h"
@@ -7,5 +8,23 @@
 
 int main (int argc, char *argv[]) {
     I2Cdev::initialize();
+    MPU6050 accelgyro ;
+    int16_t ax, ay, az;
+    int16_t gx, gy, gz;
+    if ( accelgyro.testConnection() )
+        printf("MPU6050 connection test successful\n") ;
+    else {
+        fprintf( stderr, "MPU6050 connection test failed! something maybe wrong\n");
+        return 1;
+    }
+    accelgyro.initialize();
+    printf("Device ID: %d\n", accelgyro.getDeviceID());
+    printf("  ax \t ay \t az \t gx \t gy \t gz:\n");
+    while (true) {
+        accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+        printf("  %d \t %d \t %d \t %d \t %d \t %d\r", ax, ay, az, gx, gy, gz);
+        fflush(stdout);
+        usleep(1000);
+    }
     return 0;
 }
